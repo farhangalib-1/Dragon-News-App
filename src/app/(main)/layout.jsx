ee5ad2f67c092f784df751286cb07d9a4a572055
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-05-20 03:41:08 +0600

    marquee part is implemented

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import LatestNews from '@/components/LatestNews';
 const layout = ({children}) => {
   return (
     <>
      <Header />
-        <Navbar />
+     <LatestNews/>
+    <Navbar />
       {children}
     </>
   )
--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <h2 className="text-2xl font-bold text-center">Hello NEXT-JS</h2>
+   <></>
   );
 }
