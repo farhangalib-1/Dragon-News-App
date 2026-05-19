4b7c62a69dd5327a990a97f58d6fcb0ea8b0eee1
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-05-20 03:56:31 +0600

    navbar is implemented successfully

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
+import Link from 'next/link'
 import React from 'react'
-
+import user from '@/assets/user.png'
+import Image from 'next/image'
 const Navbar = () => {
   return (
-    <div>
-        <h1>This is a Navbar</h1>
+    <div className='flex items-center justify-between w-11/12 mx-auto mt-5.5'>
+        <div></div>
+
+        <nav>
+            <ul className='flex gap-5'>
+            <li><Link href='/'>Home</Link></li>
+            <li><Link href='/about'>About</Link></li>
+            <li><Link href='/career'>Career</Link></li>
+            </ul>
+        </nav>
+        <div className='flex gap-2.25  items-center'>
+            <Image src={user} width={60} height={60} alt='user' ></Image>
+            <Link href='/login' ><button className='px-10.5 py-2.5 bg-[#403F3F] text-white font-semibold'>Login</button></Link>
+        </div>
     </div>
   )
 }
