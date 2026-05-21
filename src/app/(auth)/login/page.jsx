e705790d1cffe03340c0b8617b2fc456c4bb7b06
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-05-22 00:16:01 +0600

    Login and register form is designed

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,11 +1,30 @@
-import { montsorret } from '@/app/layout'
-import React from 'react'
+"use client"
+import Link from "next/link";
+import { useForm } from "react-hook-form"
 
-const LoginPage = ({children}) => {
+const LoginPage = () => {
+  const {register, handleSubmit, formState: { errors }} = useForm();
+  const handleFormSubmit = data => console.log(data);
   return (
-    <div className={`${montsorret.className}`}>
-        
-        <h1>This is the Login Page</h1>
+    <div className="flex items-center justify-center min-h-[80vh]"> 
+      <div>
+        <form className="text-center" onSubmit={handleSubmit(handleFormSubmit)}>
+        <label className="label">
+          <span className="label-text text-2xl font-bold mb-4">Login your account</span>
+        </label>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+  <label className="label">Email</label>
+  <input type="email" className="input" placeholder="Email" {...register("email")} />
+
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" {...register("password", { required: "Password is required", minLength: { value: 8, message: "Password must be at  8 characters" } })} />
+    {errors.password && <span className="text-error">{errors.password.message}</span>}
+  <button className="btn btn-neutral mt-4">Login</button>
+  <p className="mt-4">Don't have an account? <Link href="/resgister" className="text-secondary">Register</Link></p>
+  <p></p>
+</fieldset>
+</form>
+</div>
     </div>
   )
 }
--- src/app/(auth)/resgister/page.jsx
+++ src/app/(auth)/resgister/page.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+"use client"
+import Link from "next/link";
+import { useForm } from "react-hook-form"
 const RegisterPage = () => {
+  const {register, handleSubmit, formState: { errors }} = useForm();
+  const handleFormSubmit = data => console.log(data);
   return (
-    <div>
-      <h1>This is the Register Page</h1>
+    <div className="flex items-center justify-center min-h-[80vh]"> 
+      <div>
+        <form className="text-center" onSubmit={handleSubmit(handleFormSubmit)}>
+        <label className="label">
+          <span className="label-text text-2xl font-bold mb-4">Register your account</span>
+        </label>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+  <label className="label">Email</label>
+  <input type="email" className="input" placeholder="Email" {...register("email")} />
+
+  <label className="label">Password</label>
+  <input type="password" className="input" placeholder="Password" {...register("password", { required: "Password is required", minLength: { value: 8, message: "Password must be at  8 characters" } })} />
+    {errors.password && <span className="text-error">{errors.password.message}</span>}
+  <button className="btn btn-neutral mt-4">Register</button>
+  <p className="mt-4">Already have an account? <Link href="/login" className="text-secondary">Login</Link></p>
+  <p></p>
+</fieldset>
+</form>
+</div>
     </div>
   )
 }
